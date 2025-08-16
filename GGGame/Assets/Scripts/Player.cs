6dcb3cddc3a4a25cd58a5e0d692c43503c7cf521
Demi <148567261+Molnier@users.noreply.github.com>
2025-08-16 22:33:10 +0300

    Second Commit
    Moving Idle

--- GGGame/Assets/Scripts/Player.cs
+++ GGGame/Assets/Scripts/Player.cs
@@ -2,8 +2,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private void Update()
+    private float movingSpeed = 5f;
+    private Rigidbody2D rb;
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector.y = 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector.x = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1f;
+        }
+        rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
     }
 }
